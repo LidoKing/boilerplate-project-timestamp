bbc79386c90f03cbb6665a47d54da7e8baf1d8d9
LidoKing <kinglido203@gmail.com>
2022-01-29 18:45:18 +0800

    completed case for route with request param

--- server.js
+++ server.js
@@ -28,6 +28,24 @@ app.get("/api/1451001600000", (req, res) => {
   res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
 });
 
+app.get('/api/:date?', (req, res) => {
+  let date = req.params.date;
+
+  if (!date) {
+    let current_unix = Date.now();
+    let current_utc = new Date().toUTCString();
+    res.json({ unix: current_unix, utc: current_utc });
+  }
+
+  if (new Date(date) == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    let unix = new Date(date).getTime();
+    let utc = new Date(date).toUTCString();
+    res.json({ unix: unix, utc: utc });
+  }
+});
+
 // listen for requests :)
 var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
